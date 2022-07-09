5ae9be6193c57a4e82362bde0a45d8fde10f72d9
wakhsavior <wakhsavior@gmail.com>
2022-07-09 21:29:21 +0300

    Add HomeWork for Seminar 3

---
+++ HW_seminar_03/Task_01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+while (true)
+{
+    int int_number = new Random().Next(10000, 100000);
+    // int int_number = Convert.ToInt32(Console.ReadLine());
+
+    string str_number = Convert.ToString(int_number);
+    int length = str_number.Length;
+    int count = 0;
+
+    for (int i = 0; i < (length / 2); i++)
+    {
+        if(str_number[i]!=str_number[length-1-i])
+        {
+            Console.WriteLine($"The number {int_number} is not polindrome");
+            break;
+        }
+        count++;
+        
+    }
+
+    if(count == (length / 2)) 
+    {
+        
+        Console.WriteLine($"The number {int_number} is polindrome");
+        break;
+    }
+}
+
---
+++ HW_seminar_03/Task_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int X1 = new Random().Next(-10, 10);
+int Y1 = new Random().Next(-10, 10);
+int Z1 = new Random().Next(-10, 10);
+
+int X2 = new Random().Next(-10, 10);
+int Y2 = new Random().Next(-10, 10);
+int Z2 = new Random().Next(-10, 10);
+
+double distance = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
+
+Console.WriteLine($"Point 1- {X1}:{Y1}:{Z1}");
+Console.WriteLine($"Point 2- {X2}:{Y2}:{Z2}");
+Console.WriteLine($"Distance between 1 and 2: {Math.Round(distance * 100) / 100}");
