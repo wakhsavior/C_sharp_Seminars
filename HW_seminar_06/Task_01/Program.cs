455f3072d7e453c11af44f71e2cd299c028e8b71
wakhsavior <wakhsavior@gmail.com>
2022-07-13 22:50:25 +0300

    Add Homework Seminar 06

---
+++ HW_seminar_06/Task_01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int CountPositiveNumbes(int[] array)   //Функция по поиску количества положительных чисел в массиве, на вход принимает массив, на выход отдает количество чисел
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element > 0)
+            count++;
+    }
+    return count;
+}
+
+int[] FillArray(int M)       //Функция заполнения массива натуральными числами от -10 до 10
+{
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        array[i] = new Random().Next(-10, 11);    //Заполняем массив случайными числами для удобства делаем это Рандомайзером
+    }
+    return array;
+}
+
+Console.WriteLine("Enter the quantity of numbers: ");  //Запрос ввода количества чисел для запуска цикла по формированию массива
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];  //Подготавливаем массив для чисел
+
+array = FillArray(M);
+Console.WriteLine($"{String.Join(", ",array)}");
+
+int positiveNumbers = CountPositiveNumbes(array);
+Console.WriteLine($"The quantity of positive number: {positiveNumbers}");
+
+
+
---
+++ HW_seminar_06/Task_02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] PointLineIntersection(double k1, double b1, double k2, double b2) // Функция вычисляет координаты точки пересечения прямых линий заданных уравнениями 
+                                                //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 передаются в качестве аргументов
+                                                // Для вычисления координат приравняем функции друг к другу и найдем x, затем вычислим y
+{
+    Console.WriteLine($"k1 - {k1}, k2 - {k2}, b1 - {b1}, b2 - {b2}"); 
+    double[] coordinates = new double[2];
+    coordinates[0] = (b2 - b1) / (k1 - k2);
+    Console.WriteLine(coordinates[0]);
+    coordinates[1] = k1 * coordinates[0] + b1;
+    Console.WriteLine(coordinates[1]);
+    return coordinates;
+}
+
+double k1 = 0;
+double k2 = 0;
+double b1 = 0;
+double b2 = 0;
+
+Console.WriteLine("Вычисление координат точки пересечения прямых заданных формулами y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Введите k1:");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1:");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+b2 = Convert.ToDouble(Console.ReadLine());
+
+double[] coordinates = PointLineIntersection(k1,b1,k2, b2);
+
+Console.WriteLine($"Intersection point coordinates X: {coordinates[0]}, Y: {coordinates[1]}");
